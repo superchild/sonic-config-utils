5df0f89a6a212160637f3f7ffe39feee5afbe7a2
jimi_chen <jimi_chen@edge-core.com>
2021-07-30 08:57:32 +0800

    Add save config playbook to save SONiC and FRR's config

---
+++ ansible/save_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+usage() {
+cat << EOF
+Usage: $0 <host_file>
+EOF
+}
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+HOST_FILE=$1
+ansible-playbook -i ${HOST_FILE} playbook/save_config.yml
